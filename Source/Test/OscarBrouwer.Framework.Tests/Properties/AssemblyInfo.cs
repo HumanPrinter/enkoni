3998fab2a4a720b49bfdfc5a74281632b73d1ca9
humanprinter <humanprinter@users.sourceforge.net>
2011-01-09 20:40:32 +0100

    Added testcases for OscarBrouwer.Framework and implemented some changes as a result of those testcases

--- Source/Test/OscarBrouwer.Framework.Tests/Properties/AssemblyInfo.cs
+++ Source/Test/OscarBrouwer.Framework.Tests/Properties/AssemblyInfo.cs
@@ -1,18 +1,22 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿//--------------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Oscar Brouwer">
+//     Copyright (c) Oscar Brouwer 2010. All rights reserved.
+// </copyright>
+// <summary>
+//     Contains the project-wide metadata.
+// </summary>
+//--------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OscarBrouwer.Framework.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Contains the testcases for the OscarBrouwer.Framework assembly.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Home")]
-[assembly: AssemblyProduct("OscarBrouwer.Framework.Tests")]
-[assembly: AssemblyCopyright("Copyright © Home 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,14 +26,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6ace3742-5c0c-41f2-9e43-bca2a543cc29")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
