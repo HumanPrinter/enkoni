bc501c7b22f8e2480c458ab5b47dece42789dc97
humanprinter <humanprinter@users.sourceforge.net>
2012-03-18 18:36:13 +0100

    Updated the reference to EntityFramework to 4.3.1.0

--- Source/Enkoni.Framework.Entities.Database/Properties/AssemblyVersionInfo.cs
+++ Source/Enkoni.Framework.Entities.Database/Properties/AssemblyVersionInfo.cs
@@ -10,5 +10,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.4.1.0")]
-[assembly: AssemblyFileVersion("1.4.1.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
