6c81c728cf498135d2127ee96f14acd9dc2e06f6
humanprinter <humanprinter@users.sourceforge.net>
2012-08-26 15:23:07 +0200

    Work in progress: Unittesten voor het UI-project

---
+++ Source/Test/Enkoni.Framework.UI.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿//--------------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Oscar Brouwer">
+//     Copyright (c) Oscar Brouwer 2011. All rights reserved.
+// </copyright>
+// <summary>
+//     Contains the project-wide metadata.
+// </summary>
+//--------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Enkoni.Framework.UI.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2bd03ca1-f072-41ec-9a22-c1f60e580de3")]
+
+[assembly: CLSCompliant(true)]
---
+++ Source/Test/Enkoni.Framework.UI.Tests/UIDispatcherTest.cs
@@ -0,0 +1,50 @@
+﻿//---------------------------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="UIDispatcherTest.cs" company="Oscar Brouwer">
+//     Copyright (c) Oscar Brouwer 2012. All rights reserved.
+// </copyright>
+// <summary>
+//     Contains testcases that test the functionality of the UIDispatcher class.
+// </summary>
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+using System.Threading;
+using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Enkoni.Framework.UI.Tests {
+  /// <summary>Tests the functionality of the <see cref="UIDispatcher"/> class.</summary>
+  [TestClass]
+  public class UIDispatcherTest {
+    #region TestCases
+    /// <summary>Tests the functionality of the <see cref="UIDispatcher.BeginInvoke(System.Action)"/> class using values that should easily pass the validation.
+    /// </summary>
+    [TestMethod]
+    public void TestCase01_ValidValues() {
+      string threadName;
+      bool proceed = false;
+
+      Thread testThread = new Thread(() =>
+        UIDispatcher.BeginInvoke(() => {
+          threadName = Thread.CurrentThread.Name;
+          proceed = true;
+        })
+      );
+
+      testThread.Name = "TestThread";
+      testThread.Start();
+
+      while(!proceed) {
+        Thread.Yield();
+      }
+
+      /* Use valid values in combination with an Inclusive-Exclusive boundary combination. */
+      Assert.IsTrue(true);
+    }
+    #endregion
+  }
+
+  #region Helper classes
+  public class TestApplication : Application {
+  }
+  #endregion
+}
