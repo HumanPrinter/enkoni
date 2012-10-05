61f67b329d654627626f8e0e39da97cbf523da5d
humanprinter <humanprinter@users.sourceforge.net>
2012-10-05 16:33:29 +0200

    Made some changes to the release scripts regarding the relative paths

--- Create-Release.ps1
+++ Create-Release.ps1
@@ -11,7 +11,7 @@ $LastExitCode = 0
 Write-Host "Going to validate the release notes..." -ForegroundColor Cyan
 
 # Validate the release notes
-$messages = Validate-ReleaseNotes -xmlPath 'Source\ReleaseNotes.xml' -xsdPath 'Source\ReleaseNotes.xsd' | Select-Object Message
+$messages = Validate-ReleaseNotes -xmlPath (-join ($projectDir, '\Source\ReleaseNotes.xml')) -xsdPath (-join ($projectDir, '\Source\ReleaseNotes.xsd')) | Select-Object Message
 [bool]$proceed = $TRUE
 # Check the results to see if there were any validation messages
 foreach($message in $messages) {
@@ -31,7 +31,7 @@ if(!$proceed) {
 
 Write-Host "Validation successfull. Going to read the release notes..." -ForegroundColor Cyan
 
-$releaseInfo = Read-ReleaseNotes -path 'Source\ReleaseNotes.xml'
+$releaseInfo = Read-ReleaseNotes -path '.\Source\ReleaseNotes.xml'
 
 [int]$exitCode = $LastExitCode
 if($exitCode -ne 0) {
@@ -43,12 +43,12 @@ if($exitCode -ne 0) {
 Write-Host "Release notes successfully read. Going to prepare the working directory..." -ForegroundColor Cyan
 
 # Clear or create the working directory
-[string]$path = Join-Path $projectDir 'WorkingDir\'
+[string]$path = Join-Path $projectDir '.\WorkingDir\'
 if(Test-Path $path) {
   Clear-Directory -path $path
 }
 else {
-  New-Item -Type Directory -Path 'WorkingDir'
+  New-Item -Type Directory -Path '.\WorkingDir'
 }  
 
 Write-Host "Going to update the version numbers..." -ForegroundColor Cyan
@@ -59,7 +59,7 @@ Update-Versions -releaseInfo $releaseInfo
 Write-Host "Version numbers successfully updated. Going to build the solution..." -ForegroundColor Cyan
 
 # Build the solution
-[string]$solution = Join-Path $projectDir 'Source\Enkoni.Framework.sln'
+[string]$solution = Join-Path $projectDir '.\Source\Enkoni.Framework.sln'
 Build-Solution -solutionFile $solution -outputPath $path
 
 $exitCode = $LastExitCode
@@ -84,9 +84,9 @@ if($exitCode -ne 0) {
 Write-Host "Unit tests were successfully executed. Going to transform the release notes..." -ForegroundColor Cyan
 
 # Create the release notes
-$xmlPath = Join-Path $projectDir 'Source\ReleaseNotes.xml'
-$xslPath = Join-Path $projectDir 'Source\ReleaseNotes.xsl'
-$outputPath = Join-Path $projectDir 'WorkingDir\Release Notes.txt'
+$xmlPath = Join-Path $projectDir '.\Source\ReleaseNotes.xml'
+$xslPath = Join-Path $projectDir '.\Source\ReleaseNotes.xsl'
+$outputPath = Join-Path $projectDir '.\WorkingDir\Release Notes.txt'
 
 Write-ReleaseNotes -xml $xmlPath -xsl $xslPath -output $outputPath
 
@@ -97,7 +97,7 @@ Write-Host "Release notes were successfully transformed. Going to sign the assem
 [System.DateTime]$sign1Time = [System.DateTime]::Now
 [System.DateTime]$sign2Time = [System.DateTime]::Now
 
-Sign-Assemblies -certificate 'Source\Enkoni.Framework.pfx'
+Sign-Assemblies -certificate '.\Source\Enkoni.Framework.pfx'
 $exitCode = $LastExitCode
 if($exitCode -ne 0) {
   return
@@ -107,7 +107,7 @@ if($exitCode -ne 0) {
 
 Write-Host "Assemblies were successfully signed. Going to create the NuGet packages..." -ForegroundColor Cyan
 
-Pack-NuGet -releaseInfo $releaseInfo -outputDirectory .\WorkingDir
+Pack-NuGet -releaseInfo $releaseInfo -outputDirectory (Join-Path $projectDir '\WorkingDir')
 $exitCode = $LastExitCode
 if($exitCode -ne 0) {
   return
@@ -122,7 +122,7 @@ if(!$aliasExists) {
   Set-Alias msbuild c:\WINDOWS\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe  
 }
 
-msbuild /p:Configuration=Release /p:DumpLogOnFailure=True .\Source\Enkoni.Documentation.shfbproj
+msbuild /p:Configuration=Release /p:DumpLogOnFailure=True (Join-Path $projectDir '\Source\Enkoni.Documentation.shfbproj')
 $exitCode = $LastExitCode
 if($exitCode -ne 0) {
   Write-Host -ForegroundColor Red 'MSBuild failed. Review MSBuild-output and try again'
@@ -134,7 +134,7 @@ if($exitCode -ne 0) {
 Write-Host "The documentation was successfuly generated. Going to save the release..." -ForegroundColor Cyan
 
 [string]$productVersion = $releaseInfo.Version
-[string]$releaseDir = -join ('.\Releases\Release ', $productVersion)
+[string]$releaseDir = -join ($projectDir, '\Releases\Release ', $productVersion)
 if(!(Test-Path $releaseDir)) {
   New-Item -Type Directory -Path $releaseDir
 }
@@ -156,13 +156,13 @@ Remove-Item -Recurse -Force -Path 'WorkingDir'
 
 Write-Host "Release succeeded. Total elapsed time: "($endTime - $startTime) -ForegroundColor Green
 Write-Host "  - Release notes validation:`t`t"($validationTime - $startTime) -ForegroundColor Green
-Write-Host "  - Release notes reading:`t`t`t"($readNotesTime - $validationTime) -ForegroundColor Green
-Write-Host "  - Build solution:`t`t`t`t`t"($buildTime - $readNotesTime) -ForegroundColor Green
-Write-Host "  - Execute unit tests:`t`t`t`t"($testTime - $buildTime) -ForegroundColor Green
+Write-Host "  - Release notes reading:`t`t"($readNotesTime - $validationTime) -ForegroundColor Green
+Write-Host "  - Build solution:`t`t`t"($buildTime - $readNotesTime) -ForegroundColor Green
+Write-Host "  - Execute unit tests:`t`t`t"($testTime - $buildTime) -ForegroundColor Green
 Write-Host "  - Release notes transformation:`t"($transformTime - $testTime) -ForegroundColor Green
 Write-Host "  - Assembly signing (netto):`t`t"(($sign1Time - $transformTime) + ($signTime - $sign2Time)) -ForegroundColor Green
-Write-Host "  - NuGet package creation:`t`t`t"($nugetTime - $signTime) -ForegroundColor Green
-Write-Host "  - Build documentation:`t`t`t"($docuTime - $nugetTime) -ForegroundColor Green
+Write-Host "  - NuGet package creation:`t`t"($nugetTime - $signTime) -ForegroundColor Green
+Write-Host "  - Build documentation:`t`t"($docuTime - $nugetTime) -ForegroundColor Green
 Write-Host "  - Copying to release directory:`t"($copyTime - $docuTime) -ForegroundColor Green
 Write-Host "  - Compressing documentation:`t`t"($zipTime - $copyTime) -ForegroundColor Green
 #TODO: Push NuGet (seperate script)
