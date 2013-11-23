db64e54bacb6ac08164c33230b92d8b2b3aaa7ab
HumanPrinter <brouwer.o@gmail.com>
2013-11-23 15:27:02 +0100

    Upgraded Enkoni.Framework.Entities to .Net4 Upgraded Enkoni.Framework.Entities.File to .Net4

--- Source/Enkoni.Framework.Entities/DataSourceInfo.cs
+++ Source/Enkoni.Framework.Entities/DataSourceInfo.cs
@@ -1,6 +1,6 @@
 ﻿//---------------------------------------------------------------------------------------------------------------------------------------------------
 // <copyright file="DataSourceInfo.cs" company="Oscar Brouwer">
-//     Copyright (c) Oscar Brouwer 2012. All rights reserved.
+//     Copyright (c) Oscar Brouwer 2013. All rights reserved.
 // </copyright>
 // <summary>
 //     Defines a class that contains information about a certain datasource that is used by a repository.
--- Source/Enkoni.Framework.Entities/IEntity.cs
+++ Source/Enkoni.Framework.Entities/IEntity.cs
@@ -1,6 +1,6 @@
 ﻿//---------------------------------------------------------------------------------------------------------------------------------------------------
 // <copyright file="IEntity.cs" company="Oscar Brouwer">
-//     Copyright (c) Oscar Brouwer 2012. All rights reserved.
+//     Copyright (c) Oscar Brouwer 2013. All rights reserved.
 // </copyright>
 // <summary>
 //     Defines the basic entity API.
