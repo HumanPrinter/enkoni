a2325f5e9d59e4cb3bb93409afb8aa9e1f3b320a
HumanPrinter <brouwer.o@gmail.com>
2014-03-14 22:21:51 +0100

    Removed debug code from test case

--- Source/Test/Enkoni.Framework.Entities.Tests/BugScenarios.cs
+++ Source/Test/Enkoni.Framework.Entities.Tests/BugScenarios.cs
@@ -14,20 +14,15 @@ namespace Enkoni.Framework.Entities.Tests {
     [DeploymentItem(@"TestData\BugScenariosData.mdf", @"BugScenarios\Bug01")]
     public void Bug01_QueryById() {
       Database.SetInitializer<DatabaseRepositoryTestContext>(null);
-      AppDomain.CurrentDomain.SetData("DataDirectory", Environment.CurrentDirectory);
+      //AppDomain.CurrentDomain.SetData("DataDirectory", Environment.CurrentDirectory);
       DbContext context = new DatabaseRepositoryTestContext("BugScenarios");
-      System.Diagnostics.Trace.TraceWarning("Connection string: " + context.Database.Connection.ConnectionString);
       DataSourceInfo sourceInfo = new DatabaseSourceInfo(context);
 
       TestDomainModel domainModel = new TestDomainModel(sourceInfo);
       SubDomainModel<TestDummy> subDomain = domainModel.GetSubDomain<TestDummy>();
 
       TestDummy result = subDomain.FindEntityById(2);
-      if(result == null) {
-        System.Diagnostics.Trace.TraceWarning("Count: " + context.Set<TestDummy>().Count());
-        Console.WriteLine(context.Set<TestDummy>().Count());
-      }
-
+      
       Assert.IsNotNull(result);
       Assert.AreEqual(2, result.RecordId);
       Assert.AreEqual("TestDummy 2", result.TextValue);
