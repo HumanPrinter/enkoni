6373be336693db4db0bff9b104adfa523c0b2442
O. Brouwer <o_brouwer@hotmail.com>
2014-10-16 23:10:58 +0200

    Removed headers Refactored test class
    Related Work Items: #13, #41

--- Source/Test/Enkoni.Framework.Tests/DateTimeExtensionTest.cs
+++ Source/Test/Enkoni.Framework.Tests/DateTimeExtensionTest.cs
@@ -1,13 +1,4 @@
-﻿//---------------------------------------------------------------------------------------------------------------------------------------------------
-// <copyright file="DateTimeExtensionTest.cs" company="Oscar Brouwer">
-//     Copyright (c) Oscar Brouwer 2012. All rights reserved.
-// </copyright>
-// <summary>
-//     Contains testcases that test the functionality of the extension methods for the DateTime-struct.
-// </summary>
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.Globalization;
 using System.Threading;
 
@@ -19,47 +10,89 @@ namespace Enkoni.Framework.Tests {
   public class DateTimeExtensionTest {
     /// <summary>Tests the functionality of the <see cref="Extensions.GetWeekNumber(DateTime)"/> extension method.</summary>
     [TestMethod]
-    public void TestCase01_GetWeekNumberNoCulture() {
+    public void DateTimeExtensions_GetWeekNumber2011_01_01ReturnsWeek52() {
       Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-NL");
 
       /* January 1st 2011 is a Saturday and still is part of week 52 of 2010 */
       DateTime startValue = new DateTime(2011, 1, 1);
       int result = startValue.GetWeekNumber();
       Assert.AreEqual(52, result);
+    }
+
+    /// <summary>Tests the functionality of the <see cref="Extensions.GetWeekNumber(DateTime)"/> extension method.</summary>
+    [TestMethod]
+    public void DateTimeExtensions_GetWeekNumber2011_01_02ReturnsWeek52() {
+      Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-NL");
 
       /* January 2nd 2011 is a Sunday (the last day of the week) and still is part of week 52 of 2010 */
-      startValue = new DateTime(2011, 1, 2);
-      result = startValue.GetWeekNumber();
+      DateTime startValue = new DateTime(2011, 1, 2);
+      int result = startValue.GetWeekNumber();
       Assert.AreEqual(52, result);
+    }
+
+    /// <summary>Tests the functionality of the <see cref="Extensions.GetWeekNumber(DateTime)"/> extension method.</summary>
+    [TestMethod]
+    public void DateTimeExtensions_GetWeekNumber2011_01_03ReturnsWeek1() {
+      Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-NL");
 
       /* January 3rd 2011 is a Monday and is the first day of week 1 of 2011 */
-      startValue = new DateTime(2011, 1, 3);
-      result = startValue.GetWeekNumber();
+      DateTime startValue = new DateTime(2011, 1, 3);
+      int result = startValue.GetWeekNumber();
       Assert.AreEqual(1, result);
+    }
+
+    /// <summary>Tests the functionality of the <see cref="Extensions.GetWeekNumber(DateTime)"/> extension method.</summary>
+    [TestMethod]
+    public void DateTimeExtensions_GetWeekNumber2011_05_09ReturnsWeek19() {
+      Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-NL");
 
       /* May 9th is an arbitrary Monday in 2011 */
-      startValue = new DateTime(2011, 5, 9);
-      result = startValue.GetWeekNumber();
+      DateTime startValue = new DateTime(2011, 5, 9);
+      int result = startValue.GetWeekNumber();
       Assert.AreEqual(19, result);
+    }
+
+    /// <summary>Tests the functionality of the <see cref="Extensions.GetWeekNumber(DateTime)"/> extension method.</summary>
+    [TestMethod]
+    public void DateTimeExtensions_GetWeekNumber2011_05_15ReturnsWeek19() {
+      Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-NL");
 
       /* May 15th is an arbitrary Sunday in 2011 */
-      startValue = new DateTime(2011, 5, 15);
-      result = startValue.GetWeekNumber();
+      DateTime startValue = new DateTime(2011, 5, 15);
+      int result = startValue.GetWeekNumber();
       Assert.AreEqual(19, result);
+    }
+
+    /// <summary>Tests the functionality of the <see cref="Extensions.GetWeekNumber(DateTime)"/> extension method.</summary>
+    [TestMethod]
+    public void DateTimeExtensions_GetWeekNumber2011_12_26ReturnsWeek52() {
+      Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-NL");
 
       /* December 26nd is the first day of the last week of 2011 */
-      startValue = new DateTime(2011, 12, 26);
-      result = startValue.GetWeekNumber();
+      DateTime startValue = new DateTime(2011, 12, 26);
+      int result = startValue.GetWeekNumber();
       Assert.AreEqual(52, result);
+    }
+
+    /// <summary>Tests the functionality of the <see cref="Extensions.GetWeekNumber(DateTime)"/> extension method.</summary>
+    [TestMethod]
+    public void DateTimeExtensions_GetWeekNumber2011_12_31ReturnsWeek52() {
+      Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-NL");
 
       /* December 31st 2011 is a Saturday and is still part of week 52 of 2011 */
-      startValue = new DateTime(2011, 12, 31);
-      result = startValue.GetWeekNumber();
+      DateTime startValue = new DateTime(2011, 12, 31);
+      int result = startValue.GetWeekNumber();
       Assert.AreEqual(52, result);
+    }
+
+    /// <summary>Tests the functionality of the <see cref="Extensions.GetWeekNumber(DateTime)"/> extension method.</summary>
+    [TestMethod]
+    public void DateTimeExtensions_GetWeekNumber2012_12_31ReturnsWeek1() {
+      Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-NL");
 
       /* December 31st 2012 is a Monday and is part of week 1 of 2012 */
-      startValue = new DateTime(2012, 12, 31);
-      result = startValue.GetWeekNumber();
+      DateTime startValue = new DateTime(2012, 12, 31);
+      int result = startValue.GetWeekNumber();
       Assert.AreEqual(1, result);
     }
   }
