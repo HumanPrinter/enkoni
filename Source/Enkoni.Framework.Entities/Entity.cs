bd7fdb4e080b7eea7e5c962440c3a56a2b2646cf
Brouwer, Oscar <Oscar.Brouwer@defriesland.nl>
2015-01-23 10:44:12 +0100

    Een standaardimplementatie toegevoegd voor het kopiëren van properties van een entity op basis van AutoMapper
    Related Work Items: #46

---
+++ Source/Enkoni.Framework.Entities/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using AutoMapper;
+
+namespace Enkoni.Framework.Entities {
+  /// <summary>Provides a basic implementation of an entity-type.</summary>
+  /// <typeparam name="T">The actual entity type.</typeparam>
+  public abstract class Entity<T> : IEntity <T> {
+    /// <summary>Gets or sets the record-ID of the entity.</summary>
+    public virtual int RecordId { get; set; }
+
+    /// <summary>Copies the values from <paramref name="source"/> into this instance.</summary>
+    /// <param name="source">The entity that contains the desired values.</param>
+    public virtual void CopyFrom(T source) {
+      if(source == null) {
+        throw new ArgumentNullException("source");
+      }
+      
+      Mapper.DynamicMap(source, this, typeof(T), typeof(T));
+    }
+  }
+}
--- Source/Test/Enkoni.Framework.Entities.Tests/TestDummy.cs
+++ Source/Test/Enkoni.Framework.Entities.Tests/TestDummy.cs
@@ -7,10 +7,10 @@ using Enkoni.Framework.Serialization;
 namespace Enkoni.Framework.Entities.Tests {
   /// <summary>A helper class to support the testcases.</summary>
   [CsvRecord(IgnoreHeaderOnRead = true, WriteHeader = true, CultureName = "en-US"), XmlRoot, DebuggerDisplay("RecordId: {RecordId} TextValue: {TextValue}")]
-  public class TestDummy : IEntity<TestDummy>, ICloneable {
+  public class TestDummy : Entity<TestDummy>, ICloneable {
     /// <summary>Gets or sets a unique record ID.</summary>
     [XmlIgnore]
-    public int RecordId { get; set; }
+    public override int RecordId { get; set; }
 
     /// <summary>Gets or sets a text value.</summary>
     [CsvColumn(0), XmlElement]
@@ -23,15 +23,6 @@ namespace Enkoni.Framework.Entities.Tests {
     /// <summary>Gets or sets a value indicating whether something is true or not.</summary>
     [CsvColumn(2), XmlElement]
     public bool BooleanValue { get; set; }
-
-    /// <summary>Copies the values from <paramref name="source"/> to this instance.</summary>
-    /// <param name="source">The object that contains the correct values.</param>
-    public void CopyFrom(TestDummy source) {
-      this.RecordId = source.RecordId;
-      this.TextValue = source.TextValue;
-      this.NumericValue = source.NumericValue;
-      this.BooleanValue = source.BooleanValue;
-    }
 
     /// <summary>Creates a clone of this object.</summary>
     /// <returns>The copy of this instance.</returns>
