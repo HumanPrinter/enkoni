026399e830ac7c2af40097d2410d31549eeadff3
O. Brouwer <o_brouwer@hotmail.com>
2016-07-28 11:00:03 +0200

    Changed the unit test to use a DacPac instead of an mdf-file to make it more future proof

---
+++ Source/Test/Enkoni.Framework.Entities.Tests.Database/dbo/Tables/TestDummy.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TestDummy] (
+    [RecordId]     INT            IDENTITY (1, 1) NOT NULL,
+    [TextValue]    NVARCHAR (100) NOT NULL,
+    [NumericValue] INT            NOT NULL,
+    [BooleanValue] BIT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([RecordId] ASC)
+);
+
